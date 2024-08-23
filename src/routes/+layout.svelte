5b96f99487703fafdcb793410cb7c08c7fe63ac2
Braeden <bclarke@fastmail.com>
2024-08-23 10:58:47 -0500

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    interface Props {
+        children: any,
+    }
+    const {
+        children
+    }: Props = $props();
+</script>
+
+<div class="page-ctr">
+    <div class="nav-ctr">
+        <nav>
+            <a href="/">Home</a>
+            <a href="/servers">Servers</a>
+        </nav>
+    </div>
+    <div class="content">
+        {#if children}
+            {@render children()}
+        {/if}
+    </div>
+</div>
+
+<style>
+    .nav-ctr {
+        width: 100%;
+        text-align: center;
+        font-size: 2em;
+        background: rgb(121, 121, 121);
+        padding: 10px 0;
+    }
+    nav a {
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+    .page-ctr {
+    }
+
+    :root {
+        --select-color: #276cc7;
+    }
+</style>
